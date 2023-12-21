f322c38da3e57e34a382a058b0842245c12d5d4d
vokintru <aodplayer@bk.ru>
2023-12-21 20:21:43 +0300

    Пофикшен баг с обработкой кнопок

--- game.py
+++ game.py
@@ -18,7 +18,15 @@ class Game:
         screen.blit(img, (0, 0))
         self.scene = 0
 
-        #pygame.display.update()
+        # pygame.display.update()
+        pygame.display.flip()
+
+    def ChoiceMenu(self):
+        img = pygame.image.load('gameFiles/img/ChoiceMenu.png')
+        screen.blit(img, (0, 0))
+        self.scene = 1
+
+        # pygame.display.update()
         pygame.display.flip()
 
 
@@ -33,7 +41,18 @@ if __name__ == '__main__':
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse = pygame.mouse.get_pos()
-                if 805 <= mouse[0] <= 1115 and 595 <= mouse[1] <= 720:
-                    print("CLICKED")
+                #print(mouse)
+                if game.scene == 0:
+                    if 805 <= mouse[0] <= 1115 and 595 <= mouse[1] <= 720:  # MainMenu --> ChoiceMenu
+                        game.ChoiceMenu()
+                elif game.scene == 1:
+                    if 57 <= mouse[0] <= 345 and 45 <= mouse[1] <= 110:  # ChoiceMenu --> MainMenu
+                        game.MainMenu()
+                    if 725 <= mouse[0] <= 1065 and 410 <= mouse[1] <= 475:  # ChoiceMenu --> LvL1
+                        print("LvL1 Start")
+                    if 725 <= mouse[0] <= 1065 and 485 <= mouse[1] <= 555:  # ChoiceMenu --> LvL2
+                        print("LvL2 Start")
+                    if 725 <= mouse[0] <= 1065 and 565 <= mouse[1] <= 635:  # ChoiceMenu --> Custom LvL
+                        print("Custom LvL Start")
         clock.tick(FPS)
     pygame.quit()
