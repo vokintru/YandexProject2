1978cc46f9697b6c4497d8b802c1fe2ae3a6c8b5
vokintru <aodplayer@bk.ru>
2023-12-21 19:47:33 +0300

    Добавлена обработка нажатия на кнопку старта

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 infoObject = pygame.display.Info()
-gameDisplay = pygame.display.set_mode((infoObject.current_w, infoObject.current_h))  # Полный экран
+screen = pygame.display.set_mode((1920, 1080))  # Полный экран
 pygame.display.set_caption('Ритм Игра')
 FPS = 100
 clock = pygame.time.Clock()
@@ -11,13 +11,14 @@ clock = pygame.time.Clock()
 
 class Game:
     def __init__(self):
-        pass
+        self.scene = 0
 
     def MainMenu(self):
         img = pygame.image.load('gameFiles/img/MainMenu.png')
-        rect = img.get_rect()
-        rect.center = (infoObject.current_w/2, infoObject.current_h/2)
-        gameDisplay.blit(img, rect)
+        screen.blit(img, (0, 0))
+        self.scene = 0
+
+        #pygame.display.update()
         pygame.display.flip()
 
 
@@ -25,9 +26,14 @@ if __name__ == '__main__':
     running = True
     game = Game()
     game.MainMenu()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse = pygame.mouse.get_pos()
+                if 805 <= mouse[0] <= 1115 and 595 <= mouse[1] <= 720:
+                    print("CLICKED")
         clock.tick(FPS)
     pygame.quit()
